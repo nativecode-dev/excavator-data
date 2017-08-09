a0f597f5a66261ed0420d8557e317979c32eddbb
Mike Pham <support@nativecode.com>
2017-08-08 22:43:26 -0400

    more tests

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ import * as path from 'path'
 import * as sqlts from 'sequelize-typescript'
 
 import { ISequelizeConfig } from 'sequelize-typescript'
+import { Lincoln, Logger } from './logging'
 
 export type IDbConfiguration = ISequelizeConfig
 
@@ -17,6 +18,8 @@ export interface IConfigurationEnv {
 
 export const DefaultConfigFile: string = 'excavator.json'
 
+const log: Lincoln = Logger.extend('config')
+
 const exists = (filename: string) => new Promise((resolve, reject): void =>
   fs.exists(filename, (exist: boolean) => resolve(exist))
 )
@@ -27,25 +30,31 @@ const stringy = (value: string | undefined, defaults: string): string =>
 const stringray = (value: string | undefined, defaults: string[]): string[] =>
   value === undefined ? defaults : value.split(',')
 
-const env: string = stringy(process.env.EXCAVATOR_ENV, 'development')
-const envignore: string[] = stringray(process.env.EXCAVATOR_ENV_IGNORE, ['dev', 'development', 'local'])
-const readFile = bluebird.promisify(fs.readFile)
+const ConfigSearch = async (cwd: string, env: any): Promise<IConfiguration> => {
+  const envname: string = stringy(env.EXCAVATOR_ENV, 'development')
+  const envignore: string[] = stringray(env.EXCAVATOR_ENV_IGNORE, ['dev', 'development', 'local'])
+  const readFile = bluebird.promisify(fs.readFile)
 
-const ConfigSearch = async (cwd: string): Promise<IConfiguration> => {
   // First, let's look inside the provided path.
   const cwdpath: string = path.join(cwd, DefaultConfigFile)
+  log.debug(`Searching for config using ${cwdpath}...`)
+
   if (await exists(cwdpath)) {
     try {
+      log.debug(`Found config using ${cwdpath}`)
       const buffer: Buffer = await readFile(cwdpath)
       const configurations: IConfigurationEnv = JSON.parse(buffer.toString())
+      const config: IConfiguration = configurations[envname]
 
-      if (configurations[env]) {
-        return configurations[env]
+      if (config) {
+        log.debug(`Using environment ${envname}`, config)
+        return config
       }
     } catch (error) {
+      log.error(error)
       // If we're in production and this happens, we need to make
       // sure we throw an error.
-      if (envignore.every((e: string) => e !== env.toLowerCase())) {
+      if (envignore.every((e: string) => e !== envname.toLowerCase())) {
         throw error
       }
     }
@@ -54,15 +63,15 @@ const ConfigSearch = async (cwd: string): Promise<IConfiguration> => {
   // Finally, we'll use environment variables or defaults.
   return {
     db: {
-      dialect: stringy(process.env.EXCAVATOR_DB_DIALECT, 'sqlite'),
-      host: stringy(process.env.EXCAVATOR_DB_HOST, 'localhost'),
-      name: stringy(process.env.EXCAVATOR_DB_NAME, 'excavator'),
-      password: stringy(process.env.EXCAVATOR_DB_PASSWORD, ''),
-      storage: stringy(process.env.EXCAVATOR_DB_STORAGE, ':memory:'),
-      username: stringy(process.env.EXCAVATOR_DB_USERNAME, 'root'),
+      dialect: stringy(env.EXCAVATOR_DB_DIALECT, 'sqlite'),
+      host: stringy(env.EXCAVATOR_DB_HOST, 'localhost'),
+      name: stringy(env.EXCAVATOR_DB_NAME, 'excavator'),
+      password: stringy(env.EXCAVATOR_DB_PASSWORD, ''),
+      storage: stringy(env.EXCAVATOR_DB_STORAGE, ':memory:'),
+      username: stringy(env.EXCAVATOR_DB_USERNAME, 'root'),
     }
   }
 }
 
-export const Configuration = async (cwd?: string): Promise<IConfiguration> =>
-  await ConfigSearch(cwd || process.cwd())
+export const Configuration = async (cwd?: string, env?: any): Promise<IConfiguration> =>
+  await ConfigSearch(cwd || process.cwd(), env || process.env)
--- tests/sequelizer.ts
+++ tests/sequelizer.ts
@@ -1,20 +1,44 @@
 import 'mocha'
 
+import * as path from 'path'
+
 import { expect } from 'chai'
 import { Sequelize } from 'sequelize-typescript'
 import { Configuration, IConfiguration, Repository } from '../src/index'
 
 describe('when using sequelize-typescript loader', () => {
+  describe('loading configuration', () => {
+    it('should get default, in-memory database configuration', async () => {
+      const config: IConfiguration = await Configuration()
+      expect(config.db.name).to.equal('excavator')
+    })
+
+    it('should use config.json file', async () => {
+      const cwd: string = path.resolve(process.cwd(), 'tests/artifacts')
+      const config: IConfiguration = await Configuration(cwd)
+      expect(config.db.name).to.equal('excavator-config')
+    })
+
+    it('should use environment variables', async () => {
+      const env: any = {
+        EXCAVATOR_DB_DIALECT: 'sqlite',
+        EXCAVATOR_DB_NAME: 'excavator-env',
+        EXCAVATOR_DB_PASSWORD: '',
+        EXCAVATOR_DB_STORAGE: ':memory:',
+        EXCAVATOR_DB_USERNAME: 'root',
+      }
+      const config: IConfiguration = await Configuration(undefined, env)
+      expect(config.db.name).to.equal(env.EXCAVATOR_DB_NAME)
+    })
+
+  })
+
   describe('connecting to in-memory sqlite database', () => {
 
-    it('should open connection', (done) => {
-      Configuration()
-        .then((config: IConfiguration): Promise<Sequelize> => Repository(config))
-        .then((sequelize: Sequelize) => {
-          expect(sequelize.sync).to.be.instanceOf(Function)
-          done()
-        })
-        .catch((error: Error) => done(error))
+    it('should open connection', async () => {
+      const config: IConfiguration = await Configuration()
+      const sequelize: Sequelize = await Repository(config)
+      expect(sequelize.sync).to.be.instanceOf(Function)
     })
 
   })
