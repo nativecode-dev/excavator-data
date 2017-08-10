a87c4912507e95539a50e682e7bea65cc106e3c3
Mike Pham <support@nativecode.com>
2017-08-10 19:12:35 -0400

    cleaning up code

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,14 @@
 export * from './config'
 export * from './models/index'
+export * from './repository'
 export * from './server'
 
-import * as path from 'path'
 import * as Umzug from 'umzug'
-import * as Models from './models/index'
 
 import { Sequelize } from 'sequelize-typescript'
 import { GetConfig, IConfiguration } from './config'
 import { Logger } from './logging'
-
-const GetUmzugConfigs = (sequelize: Sequelize): Umzug.Umzug[] => {
-  const common = {
-    logging: (...args: any[]): void => Logger.debug('migrations', ...args),
-    migrations: {
-      params: [
-        sequelize.getQueryInterface(),
-        Sequelize,
-      ],
-      path: path.join(__dirname, 'migrations'),
-      pattern: /\d{14}-\w+-migration\.ts$/,
-    },
-    storage: 'sequelize',
-    storageOptions: {
-      sequelize,
-    },
-  }
-
-  const migrations = new Umzug(Object.assign({
-    logging: (...args: any[]): void => Logger.debug('migrations', ...args),
-    migrations: {
-      path: path.join(__dirname, 'migrations'),
-      pattern: /\d{14}-\w+-migration\.ts$/,
-    },
-  }, common))
-
-  const seeders = new Umzug(Object.assign({
-    logging: (...args: any[]): void => Logger.debug('seeders', ...args),
-    migrations: {
-      path: path.join(__dirname, 'seeders'),
-      pattern: /\d{14}-\w+-seeder\.ts$/,
-    },
-  }, common))
-
-  return [migrations, seeders]
-}
-
-export const GetRepository = async (config?: IConfiguration): Promise<Sequelize> => {
-  const configuration: IConfiguration = config || await GetConfig()
-
-  Logger.debug('Creating Sequelize instance', configuration.db)
-  const sequelize: Sequelize = new Sequelize(configuration.db)
-
-  const indexer: { [key: string]: any } = Models
-  const models: any[] = Object.keys(Models)
-    .filter((key: string) => indexer[key].sync)
-    .map((key: string) => indexer[key])
-
-  sequelize.addModels(models)
-  return sequelize
-}
+import { GetRepository, GetUmzugConfigs } from './repository'
 
 export const MigrateDown = async (sequelize?: Sequelize): Promise<void> => {
   const config: IConfiguration = await GetConfig()
---
+++ src/repository.ts
@@ -0,0 +1,57 @@
+import * as path from 'path'
+import * as Umzug from 'umzug'
+import * as Models from './models/index'
+
+import { Sequelize } from 'sequelize-typescript'
+import { GetConfig, IConfiguration } from './config'
+import { Logger } from './logging'
+
+export const GetUmzugConfigs = (sequelize: Sequelize): Umzug.Umzug[] => {
+  const common: Umzug.UmzugOptions = {
+    migrations: {
+      params: [
+        sequelize.getQueryInterface(),
+        Sequelize,
+      ],
+      path: path.join(__dirname, 'migrations'),
+      pattern: /\d{14}-\w+-migration\.ts$/,
+    },
+    storage: 'sequelize',
+    storageOptions: {
+      sequelize,
+    },
+  }
+
+  const migrations: Umzug.Umzug = new Umzug(Object.assign({
+    logging: (...args: any[]): void => Logger.debug('migrations', ...args),
+    migrations: {
+      path: path.join(__dirname, 'migrations'),
+      pattern: /\d{14}-\w+-migration\.ts$/,
+    },
+  }, common))
+
+  const seeders: Umzug.Umzug = new Umzug(Object.assign({
+    logging: (...args: any[]): void => Logger.debug('seeders', ...args),
+    migrations: {
+      path: path.join(__dirname, 'seeders'),
+      pattern: /\d{14}-\w+-seeder\.ts$/,
+    },
+  }, common))
+
+  return [migrations, seeders]
+}
+
+export const GetRepository = async (config?: IConfiguration): Promise<Sequelize> => {
+  const configuration: IConfiguration = config || await GetConfig()
+
+  Logger.debug('Creating Sequelize instance', configuration.db)
+  const sequelize: Sequelize = new Sequelize(configuration.db)
+
+  const indexer: { [key: string]: any } = Models
+  const models: any[] = Object.keys(Models)
+    .filter((key: string) => indexer[key].sync)
+    .map((key: string) => indexer[key])
+
+  sequelize.addModels(models)
+  return sequelize
+}
